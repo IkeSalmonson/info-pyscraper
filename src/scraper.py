ebdd424e59dd77d0a880438aa684c254dfb57a19
IkeSalmonson <lhfsalmonson@gmail.com>
2024-04-24 00:05:57 -0300

    update info_parser

--- src/scraper.py
+++ src/scraper.py
@@ -18,39 +18,39 @@ class PyScraper:
         pass  
 
 
-
-    def get_detail_url(base_url: str, html: str) :
-        """ Grab the link to the detail page of each book from the HTML code of the list page """
-        result = []
-        soup = BeautifulSoup(html, "html.parser")
-        a_tags = soup.select("article.product_pod div.image_container a")
-        for a_tag in a_tags:
-            result.append(urljoin(base_url, a_tag.get("href")))
-        return result
-
-
 ## outside of class 
+def get_detail_url(base_url: str, html: str) :
+    """ Grab the link to the detail page of each book from the HTML code of the list page """
+    result = []
+    soup = BeautifulSoup(html, "html.parser")
+    a_tags = soup.select("article.product_pod div.image_container a")
+    for a_tag in a_tags:
+        result.append(urljoin(base_url, a_tag.get("href")))
+    return result
+
+
+
 async def fetch_list(url: str) :      
-    """ Get the URL of each detail page from the list page URL """
-    #print(f"fetch_list: begin to process url: {url}")
+    """ Get the URL of each page from the list page URL """ 
     async with request("GET", url) as response:
             html = await response.text()
-            #urls = get_detail_url(url, html)
+#urls = get_detail_url(url, html)
     #return urls
     return html 
 
 
-async def asyncio_main(scrape_url ):
+async def asyncio_main(scrape_urls ):
     """ Implementing web scraping by using asyncio   """
-    detail_urls, phone_list = [], [] 
-    fetch_list_tasks = [ asyncio.create_task(fetch_list( url  )) for url in scrape_url ]
+    info_pages, phone_list = [], [] 
+    fetch_list_tasks = [ asyncio.create_task(fetch_list( url  )) for url in scrape_urls ]
     for urls in asyncio.as_completed(fetch_list_tasks):
-        detail_urls.extend(await urls)
+        info_pages.extend(await urls)
 
-    for item in detail_urls:
-        file = open('read.txt', 'w') 
-        file.write(str(detail_urls)) 
-        file.close() 
+    fetch_info_tasks = [asyncio.create_task(scrape_info(info_pages))]
+    for item in info_pages:
+        print('type item', type(item))
+        print(' item', (item))
+
         phone_list.extend(find_contact_tel(item))
     return phone_list   
 
