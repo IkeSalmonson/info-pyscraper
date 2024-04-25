f61e9a4ab733968b1076284a8600b10513eb329f
IkeSalmonson <lhfsalmonson@gmail.com>
2024-04-25 00:21:40 -0300

    rename some files

--- src/info_parser.py
+++ src/info_parser.py
@@ -2,46 +2,40 @@
 import re 
 import sys 
 
-class InfoParse:
 
 
-    def __init__(self ):
-        pass 
-       
-       
-
+import logging
+  
+def validateURL(  url):
+    regex = "^((http|https)://)[-a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)$"
+    r = re.compile(regex)
+    if (re.search(r, url)):
+        return url , True 
+    else:
+        return url , False
  
-    def validateURL(self, url):
-        regex = "^((http|https)://)[-a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)$"
-        r = re.compile(regex)
-        if (re.search(r, url)):
-            return url , True 
-        else:
-            return url , False
+def format_input( input_string):
+    #split input by line 
+    input_string_line = [ line for line in input_string.split('\n')  ]
  
-    def format_input(self, input_string):
-        #split input by line 
-        input_string_line = [ line for line in input_string.split('\n')  ]
-        #validate if url 
-        not_a_url = []
-        url_list = []
-        for line in input_string_line  :
-            validated_string , is_url =  self.validateURL(line)
-            if is_url:
-                url_list.append(validated_string)
-            else: 
-                not_a_url.append(validated_string)
-
-        return url_list, not_a_url
+    #validate if url 
+    not_a_url = []
+    url_list = []
+    for line in input_string_line  :
+        validated_string , is_url =  validateURL(line)
+        if is_url:
+            url_list.append(validated_string)
+        else: 
+            not_a_url.append(validated_string)
+    return url_list, not_a_url
      
-    def find_logo_image(self, html_string ):
-        re.Matchs(self.html_string , "*logo*") 
+def find_logo_image(  html_string ):
+    re.Matchs(html_string , "*logo*") 
         # html tag Logo 
         # img link contains logo 
         # img link contains company name 
         # img link leads to / or /home or home/*
-
-        raise TypeError("Logo image not found ")
+    return("Logo image not found ")
     
 
 def find_contact_tel(  html_string ):
--- src/scraper.py
+++ src/scraper.py
@@ -1,9 +1,13 @@
- 
 import asyncio
 from aiohttp import request
 from aiomultiprocess import Pool
 from bs4 import BeautifulSoup
 import re 
+import sys 
+
+
+import logging
+ 
 
 from info_parser import find_contact_tel
 
@@ -18,20 +22,7 @@ class PyScraper:
     def test_func(self):
         print('test func works ')
         pass  
-
-
-## outside of class 
-def WORKINget_scraped_info(base_url: str, html: str) :
-    """ Grab the link to the detail page of each book from the HTML code of the list page """
-    print('get_scraped_info')
-    result = []
-    soup = BeautifulSoup(html, "html.parser")
-    print('base_url ', base_url)
-    logo_tags = soup.select('img')
-    print('logo_tags', logo_tags)    
  
-    return logo_tags
-
 ########
 
 def get_scraped_info(base_url: str, html: str) :
@@ -39,24 +30,34 @@ def get_scraped_info(base_url: str, html: str) :
     print('get_scraped_info')
     result = []
     soup = BeautifulSoup(html, "html.parser")
-    print('base_url ', base_url)
-    logo_candidate = soup.find_all(href=re.compile("logo"), id='link1')
-    logo_tags = soup.select('img')
-    print('logo_tags in getscraped', logo_tags)   
+    print('base_url %s ' % base_url)
+    logo_candidate = soup.find_all(href=re.compile("logo") )
+    print('len logo_candidate> ', len(logo_candidate))
+    regex_phone_candidate = r"(?:([+]\d{1,4})[-.\s]?)?(?:([(]\d{1,3}[)])[-.\s]?)?(\d{1,4})[-.\s]?(\d{1,4})[-.\s]?(\d{1,9})"
+    contact_tel_candidate = soup.find_all(text=re.compile(regex_phone_candidate) )
+    print('len contact_tel_candidate  ', len(contact_tel_candidate))   
  
-    return logo_tags
+    return logo_candidate
 
 #####
 
 
 async def fetch_list(url: str) :      
-    """ Get the URL of each page from the list page URL """ 
+    """ Get the URL of each page from the URL list and scrap info  """ 
     print('fetch_list >> ', url )
-    scraped_info_list =[]
-    async with request("GET", url) as response:
+    scraped_info_list =[]    
+    try: 
+        headers = {"user-agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"}
+        async with request("GET", url , headers = headers) as response:
+            print('response status ', response.status)
+            assert response.status == 200
             html = await response.text()
             print('before call get_scraped_info')
             scraped_info_list.append(   get_scraped_info(url, html) )
+    except Exception as err:
+        print('Exception', err , 'Exception Type:', type(err), file=sys.stderr) 
+        
+
     return scraped_info_list 
  
  
@@ -64,27 +65,28 @@ async def fetch_list(url: str) :
 ##### using async for develpment  
 
 
-async def asyncio_main(scrape_urls ):
+async def asyncio_main(scrape_urls: list ):
     """ Implementing web scraping by using asyncio   """
-    info_pages    = [] 
+    print('asyncio_main')
+    url_infos    = [] 
     fetch_list_tasks = [ asyncio.create_task(fetch_list( url  )) for url in scrape_urls ]
     for urls in asyncio.as_completed(fetch_list_tasks):
-        info_pages.extend(await urls)
+        url_infos.extend(await urls)
  
-    return info_pages   
+    return url_infos   
 
 
 
 ####### using multiprocess async 
-async def aiomultiprocess_main(scrape_urls):
+async def aiomultiprocess_main(scrape_urls: list):
     """ Integrating multiprocessing and asyncio with the help of aiomultiprocess using Pool method """
-    info_pages  = [] 
-    print('before poll loop')
-    async with Pool() as pool: 
-        print('in poll loop')
+    url_infos  = [] 
+ 
+    async with Pool() as pool:  
         async for urls in pool.map(fetch_list,[ url for url in scrape_urls ]):
-            info_pages.extend(urls)
-            print('in poll loop scrape_urls '  , scrape_urls)
-    print('aiomultiprocess_main info_pages' , info_pages)
+            url_infos.extend(urls)
+  
+    print('aiomultiprocess_main info_pages' , url_infos)
+    return url_infos
  
  
